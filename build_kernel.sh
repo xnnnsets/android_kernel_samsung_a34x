648c636aff458702ed26ef615b94d12c6108e9c9
xnnnsets <muhamadsolihin019501@gmail.com>
2025-05-26 16:10:57 +0700

    import a34x_defconfig_susfs

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-export PATH=$(pwd)/toolchain/clang/host/linux-x86/clang-r522817/bin:$PATH
-export CROSS_COMPILE=$(pwd)/toolchain/clang/host/linux-x86/clang-r522817/bin/aarch64-linux-gnu-
-export CC=$(pwd)/toolchain/clang/host/linux-x86/clang-r522817/bin/clang
+export PATH=$(pwd)/toolchain/clang/host/linux-x86/clang-r547379/bin:$PATH
+export CROSS_COMPILE=$(pwd)/toolchain/clang/host/linux-x86/clang-r547379/bin/aarch64-linux-gnu-
+export CC=$(pwd)/toolchain/clang/host/linux-x86/clang-r547379/bin/clang
 export CLANG_TRIPLE=aarch64-linux-gnu-
 export ARCH=arm64
 export PLATFORM_VERSION=14
@@ -14,7 +14,5 @@ export TARGET_PRODUCT=a34xxx
 export KCFLAGS=-w
 export CONFIG_SECTION_MISMATCH_WARN_ONLY=y
 
-make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y LLVM=1 LLVM_IAS=1 a34x_defconfig
-make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y LLVM=1 LLVM_IAS=1 -j16
-
-cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
+make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y LLVM=1 LLVM_IAS=1 a34x_susfs_defconfig
+make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y LLVM=1 LLVM_IAS=1 -j6
